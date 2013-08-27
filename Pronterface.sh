bb5df6ac1bdf1bffd5c30e4a4c1c29055482f548
Joshua Smith <josh@hackerlab.org>
2013-08-27 10:07:19 -0700

    Create Pronterface.sh

---
+++ Pronterface.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This script will open Pronterface. If program does not work, check #the path to the program. Also change permissions to make script run.
+cd ~/Printrun-master
+gksudo python pronterface.py
+
+#Created by Joshua Smith @joshiesmithy for Hacker Lab.
