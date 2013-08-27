a003f2dbd055fc5def416a4a51b1dccead8f14d6
Joshua Smith <josh@hackerlab.org>
2013-08-27 10:08:10 -0700

    Create Slic3r.sh

---
+++ Slic3r.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#This script will open Slic3r program. If program does not work, check 
+#the path to the program. Also change permissions to make script run.
+cd ~/Slic3r
+./slic3r.pl
+
+#Created by Joshua Smith @joshiesmithy for Hacker Lab.
